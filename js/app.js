8a682630e4d37c5af82a14b5b14775f8a30c88b5
Simon Pedersen <mail@sjkp.dk>
2014-03-28 01:04:50 +0100

    Gets config from blob storage.

--- js/app.js
+++ js/app.js
@@ -5,13 +5,12 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var cloudservice = require('./routes/cloudservice');
 var http = require('http');
 var path = require('path');
-
 var app = express();
-
+var dotenv = require('dotenv');
+dotenv.load();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -25,16 +24,16 @@ app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
-app.get('/cloudservice', cloudservice.list);
-app.get('/cloudservice/delete', cloudservice.delete);
-app.get('/cloudservice/deploy', cloudservice.deploy);
+app.get('/cloudservice/:id', cloudservice.list);
+app.get('/cloudservice/:id/delete', cloudservice.delete);
+app.get('/cloudservice/:id/deploy', cloudservice.deploy);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
